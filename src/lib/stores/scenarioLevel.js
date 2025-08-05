750de7a4116bb5895df88bb083c8307b995a590d
Christopher J. Wells <chris@redfinsolutions.com>
2025-08-04 21:15:55 -0400

    svg icons for conditions, some attributes

---
+++ src/lib/stores/scenarioLevel.js
@@ -0,0 +1,15 @@
+import { writable } from 'svelte/store';
+
+const initial =
+	typeof localStorage !== 'undefined' && localStorage.getItem('scenarioLevel')
+		? JSON.parse(localStorage.getItem('scenarioLevel'))
+		: null;
+
+export const scenarioLevel = writable(initial);
+
+// Save on change
+if (typeof localStorage !== 'undefined') {
+	scenarioLevel.subscribe((value) => {
+		localStorage.setItem('scenarioLevel', JSON.stringify(value));
+	});
+}
