9f97fd2ad57e27acb96ae66712a239aff0a39737
Christopher J. Wells <chris@redfinsolutions.com>
2025-08-18 13:15:53 -0400

    add GitHub Actions deployment to GitHub Pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,20 @@
-import adapter from '@sveltejs/adapter-node';
+import adapter from '@sveltejs/adapter-static';
+
+const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
-const config = { kit: { adapter: adapter() } };
+const config = {
+	kit: {
+		adapter: adapter({
+			fallback: undefined, // can be null if you're building a SPA
+			pages: 'build',
+			assets: 'build',
+			strict: false
+		}),
+		paths: {
+			base: dev ? '' : process.env.BASE_PATH
+		}
+	}
+};
 
 export default config;
